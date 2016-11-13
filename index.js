8e8e5f01b568ac1dc66af8d2ffb53d7293ba7744
Paul R <paul.regine@ymail.com>
2016-11-13 22:22:42 +0100

    refactor

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (request, response) {
-  response.render('pages/index');
+app.get('/', function(request, response) {
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
